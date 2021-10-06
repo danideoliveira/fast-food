31e42ed9ee9c5d6593c62f452047a5a5f3d01d0b
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2021-10-06 17:40:35 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var formidable = require('formidable');
+
+
+http.createServer(
+    function(req, res) {
+        if (req.url == '/total') {
+            var form = new formidable.IncomingForm();
+
+            form.parse(req,
+                function(err, fields, files) {
+
+                    var h = Number(fields.hamburguer)
+                    var q = Number(fields.queijo)
+                    var s = Number(fields.salada)
+                    var a = Number(fields.acompanhamento)
+                    var b = Number(fields.bebida)
+
+                    var total = 4 + h + q + s + a + b
+                      
+                    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                    res.write(`<div>`);
+                    res.write(`<h1>Valor Total</h1>`);
+                    res.write(`<br>`);
+                    res.write(`<p>R$ ${total.toFixed(2)}</p>`);                            res.write(`<br>`);
+                    res.write(`<br>`);
+                    res.write(`<input type="submit" value="Confirmar Pedido">`);
+                    res.write(`<br>`);
+                    res.write(`<a href="http://localhost:8080/">Cancelar</a>`);
+                    res.write(`<br>`);
+                    res.write(`</div>`);
+                    res.end();
+                    
+                }
+            );
+        }
+        else {
+            fs.readFile('index.html',
+                function(err, pagina) {
+                    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+                    res.write(pagina);
+                    res.end();
+                } 
+            );
+        }
+    }
+).listen(8080);
+
+console.log('Servidor iniciado na porta 8080. Pressione Ctrl + C para encerrar.');
